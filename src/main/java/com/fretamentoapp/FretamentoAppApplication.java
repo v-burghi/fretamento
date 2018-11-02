04c8eada673391e0afe7b54b8e214ddda489ce5a
vhbatista <vhbatista@stefanini.com>
2018-11-02 19:50:21 -0300

    inicial

---
+++ src/main/java/com/fretamentoapp/FretamentoAppApplication.java
@@ -0,0 +1,12 @@
+package com.fretamentoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FretamentoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FretamentoAppApplication.class, args);
+	}
+}
